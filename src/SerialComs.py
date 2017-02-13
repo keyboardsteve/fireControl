b849cdee92683c14bd41313d8b25cfaa62596bb2
keyboardsteve <keyboardsteve@gmail.com>
2017-02-12 20:25:25 -0600

    Added posix OS support to the serial comms file

--- src/SerialComs.py
+++ src/SerialComs.py
@@ -2,6 +2,7 @@ import wx
 from wx.lib.pubsub import pub
 
 import threading
+import os
 
 import serial
 import serial.tools.list_ports as lp
@@ -16,9 +17,14 @@ class SerialComs():
         #self.xbee.port = "COM3"  #THIS NEED TO AUTOMATICALLY FIND THE SERIAL DEVICE SOMEHOW...
         serialPorts = lp.comports()
         for port in serialPorts:
-            if "VID:PID=0403:6015" in port.hwid: #The PID number in this line may be overkill.  We probably dont need to compare that
-                self.xbee.port = port.device
-                break
+            if os.name == "nt":
+                if "VID:PID=0403:6015" in port.hwid: #The PID number in this line may be overkill.  We probably dont need to compare that
+                    self.xbee.port = port.device
+                    break
+            elif os.name == "posix":
+                if "VID:PID=0403:6015" in port[2]:
+                    self.xbee.port = port[0]
+                    break
         self.xbee.open()
         self.t = threading.Thread(target=self.recv)
         self.t.daemon = True
